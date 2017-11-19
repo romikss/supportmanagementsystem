79c77cb626ec933596c8f0a699583fd2489be40a
Roman Sydorenko <romikss@gmail.com>
2017-11-19 03:02:43 +0000

    Update SupportDayController.cs

--- SupportManagementSystem/Controllers/SupportDayController.cs
+++ SupportManagementSystem/Controllers/SupportDayController.cs
@@ -6,7 +6,7 @@ using System.Linq;
 namespace SupportManagementSystem.Controllers
 {
     [Produces("application/json")]
-    [Route("api/supportday")]
+    [Route("api/supportdaytest")]
     public class SupportDayController : Controller
     {
         private ISupportSlotRepository _supportSlotRepository;
