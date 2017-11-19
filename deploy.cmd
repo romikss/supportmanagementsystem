82d0bbe63d1766be4cba4e0598f36437524af0cc
Roman Sydorenko <romikss@gmail.com>
2017-11-19 03:01:31 +0000

    Update deploy.cmd

--- deploy.cmd
+++ deploy.cmd
@@ -2,4 +2,7 @@
 call nuget.exe restore "%DEPLOYMENT_SOURCE%\SupportManagementSystem.sln" -MSBuildPath "%MSBUILD_15_DIR%"
 
 :: 2. Build and publish
-call "%MSBUILD_15_DIR%\MSBuild.exe" "%DEPLOYMENT_SOURCE%\SupportManagementSystem.sln" /t:SupportManagementSystem /p:DeployOnBuild=true /p:configuration=Release /p:publishurl="%DEPLOYMENT_TARGET%"
+call "%MSBUILD_15_DIR%\MSBuild.exe" "%DEPLOYMENT_SOURCE%\SupportManagementSystem.sln" /t:SupportManagementSystem /p:DeployOnBuild=true /p:configuration=Release /p:publishurl="%DEPLOYMENT_TEMP%"
+
+:: 3. KuduSync
+call "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_TEMP%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
